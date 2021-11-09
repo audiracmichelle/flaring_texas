337d99ef1f233c59a4131d41dd9c620f993a4c85
Michelle Audirac <audiracmichelle@michelles-air.lan>
2021-11-09 13:46:52 -0600

    included script that simulates particles

--- disperser_script.r
+++ disperser_script.r
@@ -4,12 +4,12 @@ library(disperseR)
 library(USAboundaries)
 library(USAboundariesData)
 #library(tidycensus)
-#census_api_key('***REMOVED***')
+#### include your census api key
+#census_api_key('<your_key>')
 
 ######## ######## ######## ########
-# data
-#
-# set /home/kitematic as working directory
+# 
+# set /home/kitematic as working directory in docker container
 #
 ######## ######## ######## ########
 
@@ -86,7 +86,7 @@ length(grep("/home/rstudio", run_log))
 # the output is a list that contains strings. The strings describe the location of the hysplit output
 
 ######## ######## ######## ########
-# test link_units
+# link_units
 ######## ######## ######## ########
 lapply(disperseR::units, class)
 lapply(input.refs, summary)
@@ -100,12 +100,21 @@ year.mons <- disperseR::get_yearmon(start.year = "2017",
 pbl.height = NULL
 pbl_trim = FALSE
 
-
-
 counties. = USAboundaries::us_counties( ) %>%
   filter(statefp == "48")
 
-tracts_sf <- st_read("./data/local/tl_2016_48_tract/tl_2016_48_tract.shp") 
+# # python
+# import wget
+# from zipfile import ZipFile
+# import os
+# 
+# url = 'https://www2.census.gov/geo/tiger/TIGER2016/TRACT/tl_2016_48_tract.zip'
+# wget.download(url, os.path.expanduser('~/tmp'))
+# file_name = os.path.expanduser('~/tmp/tl_2016_48_tract.zip')
+# ZipFile(file_name, 'r').extractall(os.path.expanduser('~/tmp/tl_2016_48_tract/'))
+# os.system("ls ~/tmp/tl_2016_48_tract/")
+
+tracts_sf <- st_read("~/tmp/tl_2016_48_tract/tl_2016_48_tract.shp") 
 tracts_sf %<>% 
   filter(STATEFP == "48") %>% 
   rename(statefp = STATEFP, 
@@ -122,31 +131,14 @@ lapply(counties., class)
 class(tracts_sf)
 lapply(tracts_sf, class)
 
-
 crosswalk. = NULL
 duration.run.hours = 12
 overwrite = FALSE
 
-####
-# link units that have emissions in each yyyymm
-
-length(unique(input.refs$ID))
-
-xx <- input.refs %>% 
-  mutate(month = month(date)) %>% 
-  distinct(ID, month) %>% 
-  group_by(ID) %>% 
-  summarise(n = n()) %>% 
-  filter(n == 12) %>% 
-  pull(ID)
-length(xx)
-
 units.run <- input.refs %>%
-  distinct(ID, uID, Latitude, Longitude, Height, year) %>% 
-  filter(ID %in% xx) 
+  distinct(ID, uID, Latitude, Longitude, Height, year)
 units.run <- data.table(units.run, stringsAsFactors = FALSE)
 
-# link all units to counties
 linked_counties <- disperseR::link_all_units(
   units.run = units.run,
   link.to = link.to,
@@ -163,169 +155,3 @@ linked_counties %<>%
   na.omit()
 
 write_rds(linked_counties, "linked_counties.rds")
-
-####
-# link all units
-
-length(unique(input.refs$ID))
-
-# xx <- input.refs %>% 
-#   mutate(month = month(date)) %>% 
-#   distinct(ID, month) %>% 
-#   group_by(ID) %>% 
-#   summarise(n = n()) %>% 
-#   filter(n == 12) %>% 
-#   pull(ID)
-# length(xx)
-
-units.run <- input.refs %>%
-  distinct(ID, uID, Latitude, Longitude, Height, year) #%>% 
-  #filter(!ID %in% xx) 
-units.run <- data.table(units.run, stringsAsFactors = FALSE)
-
-source("link_all_units.R")
-
-# link all units to counties
-linked_counties <- link_all_units_(
-  units.run = units.run,
-  link.to = link.to,
-  mc.cores = mc.cores,
-  year.mons = year.mons,
-  #pbl.height = pblheight,
-  pbl_trim = pbl_trim,
-  counties. = tracts_sf,
-  crosswalk. = crosswalk.,
-  duration.run.hours = duration.run.hours,
-  overwrite = overwrite)
-
-linked_counties %<>% 
-  na.omit()
-
-write_rds(linked_counties, "linked_counties.rds")
-
-#### ####
-tract_sf <- get_acs(year = 2018,
-                     geography = "tract",
-                     variables = "B01001_001",
-                     state = "TX",
-                     geometry = TRUE)
-tract_sf %<>% 
-  rename(pop = estimate, 
-         tract_name = NAME) %>% 
-  select(GEOID, tract_name, pop)
-
-crs_ <- "+proj=aea +lat_1=20 +lat_2=60 +lat_0=40 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m"
-
-tract_sf <- st_transform(tract_sf, crs_)
-  
-counties = counties.
-res.link = 12000
-res.link. = res.link
-pbl.trim = FALSE
-pbl. = pbl.trim
-return.linked.data = TRUE
-
-particles <- list()
-
-units.run <- input.refs %>%
-  distinct(ID, uID, Latitude, Longitude, Height, year)
-units.run <- unique( units.run[, .( uID, ID)])
-#unit <- units.run[1,]
-for(i in 1:nrow(units.run)) {
-  unit <- units.run[which(units.run$ID == "48177000200"),]
-  d <- list()
-  for(x in year.mons) {
-    print(x)
-    month_YYYYMM = "20179"
-    #month_YYYYMM = yearmons[1]
-    
-    start.date <-
-      as.Date(paste(
-        substr(month_YYYYMM, 1, 4),
-        substr(month_YYYYMM, 5, 6),
-        '01',
-        sep = '-'
-      ))
-    end.date <-
-      seq(start.date, by = paste (1, "months"), length = 2)[2] - 1
-    
-    ## name the eventual output file
-    #output_file <-
-    #  file.path( ziplink_dir,
-    #             paste0("countylinks_", unit$ID, "_", start.date, "_", end.date, ".fst"))
-    
-    ## identify dates for hyspdisp averages and dates for files to read in
-    vec_dates <-
-      as(
-        seq.Date(
-          as.Date(start.date),
-          as.Date(end.date),
-          by = '1 day'),
-        'character')
-    vec_filedates <-
-      seq.Date(
-        from = as.Date( start.date) - ceiling( duration.run.hours / 24), # <----- validate
-        to = as.Date( end.date),
-        by = '1 day'
-      )
-    
-    ## list the files
-    pattern.file <-
-      paste0( '_',
-              gsub( '[*]', '[*]', unit$ID),
-              '_(',
-              paste(vec_filedates, collapse = '|'),
-              ').*\\.fst$'
-      )
-    hysp_dir.path <-
-      file.path( hysp_dir,
-                 unique( paste( year( vec_filedates),
-                                formatC( month( vec_filedates), width = 2, flag = '0'),
-                                sep = '/')))
-    files.read <-
-      list.files( path = hysp_dir.path,
-                  pattern = pattern.file,
-                  recursive = F,
-                  full.names = T)
-    
-    ## read in the files
-    d_ <- lapply(files.read, read.fst, as.data.table = TRUE)
-    
-    # if( length( d) == 0)
-    #   return( paste( "No files available to link in", month_YYYYMM))
-    # print(  paste( Sys.time(), "Files read and combined"))
-    
-    ## Combine all parcels into single data table
-    d_ <- rbindlist(d_)
-    
-    d[[x]] <- na.omit(d_)
-  }
-  
-  d <- bind_rows(d)
-  
-  Pdate_ <- unique(d$Pdate)
-  d %<>% 
-    group_by(Pdate) %>% 
-    summarise(particles = n())
-  
-  
-  
-  # particles_ <- tract_sf
-  # particles_$count <- lengths(st_intersects(tract_sf, d))
-  # particles_ %<>% 
-  #   filter(count > 0)
-  particles[[unit$ID]] <- d
-}
-
-particles <- bind_rows(particles)
-
-particles <- spTransform(
-  SpatialPointsDataFrame(coords = particles[, .(lon, lat)],
-                         data = d,
-                         proj4string = CRS( "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0")),
-  p4s
-) %>% 
-  st_as_sf()
-
-write_rds(d, "particles.rds")
-
