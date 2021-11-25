ff309cc0a63de2f578a1d1a0f31b20a487d1be28
Michelle Audirac <audiracmichelle@michelles-air.lan>
2021-11-25 12:35:40 -0600

    changed to sbatch

--- jobs/launcher_run_disperser.sh
+++ jobs/launcher_run_disperser.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 #SBATCH -J run_disperser_parallel            # job name
 #SBATCH -N 1                        # number of nodes requested
-#SBATCH -n 1                       # total number of tasks to run in parallel
-#SBATCH -p skx                  # queue (partition) 
+#SBATCH -n 4                       # total number of tasks to run in parallel
+#SBATCH -p skx-normal                  # queue (partition) 
 #SBATCH -t 08:00:00                 # run time (hh:mm:ss) 
 
 module load tacc-singularity
 
 export LD_PRELOAD=""
 
-srun singularity exec disperser_latest.sif Rscript --vanilla jobs/run_disperser.R -y 2015 -n 1000 -w "/work/08317/m1ch3ll3/stampede2/flaring_texas"
-srun singularity exec disperser_latest.sif Rscript --vanilla jobs/run_disperser.R -y 2018 -n 1000 -w "/work/08317/m1ch3ll3/stampede2/flaring_texas"
-srun singularity exec disperser_latest.sif Rscript --vanilla jobs/run_disperser.R -y 2019 -n 1000 -w "/work/08317/m1ch3ll3/stampede2/flaring_texas"
-srun singularity exec disperser_latest.sif Rscript --vanilla jobs/run_disperser.R -y 2020 -n 1000 -w "/work/08317/m1ch3ll3/stampede2/flaring_texas"
+module load launcher
+
+export LD_PRELOAD=""
+export LAUNCHER_WORKDIR=/work/08317/m1ch3ll3/stampede2/flaring_texas
+export LAUNCHER_JOB_FILE=jobs/jobs_run_disperser
+
+${LAUNCHER_DIR}/paramrun
