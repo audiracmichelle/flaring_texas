a849a0ed5ea1803eb84cee592d261aeaf752bdca
Michelle Audirac <m1ch3ll3@login2.stampede2.tacc.utexas.edu>
2022-01-31 17:58:48 -0600

    running missing runs

--- jobs/launcher_run_disperser.sh
+++ jobs/launcher_run_disperser.sh
@@ -1,19 +1,14 @@
 #!/bin/sh
 #SBATCH -J run_disperser            # job name
-#SBATCH -N 2                        # number of nodes requested
-#SBATCH -n 100                       # total number of tasks to run in parallel
+#SBATCH -N 4                        # number of nodes requested
+#SBATCH -n 24                       # total number of tasks to run in parallel
 #SBATCH -p skx-dev                  # queue (partition) 
-#SBATCH -t 02:00:00                 # run time (hh:mm:ss) 
-
-module load tacc-singularity
-
-export LD_PRELOAD=""
+#SBATCH -t 00:30:00                 # run time (hh:mm:ss) 
 
 module load launcher
-
-export LD_PRELOAD=""
 export LAUNCHER_WORKDIR=/work/08317/m1ch3ll3/stampede2/flaring_texas
-export LAUNCHER_JOB_FILE=jobs/jobs_run_disperser_2015
+export LAUNCHER_JOB_FILE=jobs/jobs_run_disperser
+export LD_PRELOAD=""
 
 ${LAUNCHER_DIR}/paramrun
 
