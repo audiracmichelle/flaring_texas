4308ab50a115ae9e1ea91da4b86034da372d9a5a
Michelle Audirac <audiracmichelle@michelles-air.lan>
2022-01-31 07:29:42 -0600

    modified number of parallel tasks

--- jobs/launcher_run_disperser.sh
+++ jobs/launcher_run_disperser.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #SBATCH -J run_disperser            # job name
 #SBATCH -N 1                        # number of nodes requested
-#SBATCH -n 1                       # total number of tasks to run in parallel
+#SBATCH -n 100                       # total number of tasks to run in parallel
 #SBATCH -p skx-dev                  # queue (partition) 
-#SBATCH -t 01:00:00                 # run time (hh:mm:ss) 
+#SBATCH -t 04:00:00                 # run time (hh:mm:ss) 
 
 module load tacc-singularity
 
@@ -13,7 +13,7 @@ module load launcher
 
 export LD_PRELOAD=""
 export LAUNCHER_WORKDIR=/work/08317/m1ch3ll3/stampede2/flaring_texas
-export LAUNCHER_JOB_FILE=jobs/jobs_run_disperser
+export LAUNCHER_JOB_FILE=jobs/jobs_run_disperser_2015
 
 ${LAUNCHER_DIR}/paramrun
 
