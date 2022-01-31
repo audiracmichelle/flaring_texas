b8fbffa434d6aac4f301bf15615e0bcfbd7bf755
Michelle Audirac <audiracmichelle@michelles-air.lan>
2022-01-31 16:20:18 -0600

    included chunk start and end option

--- jobs/run_disperser.R
+++ jobs/run_disperser.R
@@ -4,7 +4,11 @@ library(argparse)
 parser <- ArgumentParser()
 parser$add_argument("-y", "--year", default=2015,
                     help="Year to run", type="integer")
-parser$add_argument("-n", "--n_chunks", default=200,
+parser$add_argument("-start", "--start", default=NULL,
+                    help="Chunk startpoint", type="integer")
+parser$add_argument("-end", "--end", default=NULL,
+                    help="Chunk endpoint", type="integer")
+parser$add_argument("-n", "--n_chunks", default=20,
                     help="Total number of chunks of runs", type="integer")
 parser$add_argument("-c", "--chunk", default=NULL,
                     help="Chunk to be run", type="integer")
@@ -13,9 +17,11 @@ parser$add_argument("-w", "--wkdir", default=NULL,
 args = parser$parse_args()
 
 # args=list()
-# args$year = as.integer(2017)
-# args$n_chunks = as.integer(1000)
-# args$chunk = as.integer(1000)
+# args$year = as.integer(2016)
+# args$start = NULL
+# args$end = NULL
+# args$n_chunks = as.integer(20)
+# args$chunk = NULL
 # args$wkdir = "/work/08317/m1ch3ll3/stampede2/flaring_texas"
 
 ####
@@ -60,11 +66,12 @@ disperseR::get_data(data = "metfiles",
 ####
 # define chunks and run_disperser_parallel inputs
 
-N = nrow(input)
-chunk_seq = round(seq(0,N,length.out=(args$n_chunks + 1)))
+if(is.null(args$start)){start = 0} else {start = args$start}
+if(is.null(args$end)){end = nrow(input)} else {end = args$end}
+chunk_seq = round(seq(start,end,length.out=(args$n_chunks + 1)))
 
 if(!is.null(args$chunk)) {
-  chunk_seq = c(chunk_seq[args$chunk] + 1, chunk_seq[args$chunk+1])  
+  chunk_seq = c(chunk_seq[args$chunk], chunk_seq[args$chunk+1])  
 }
 
 pbl.height = NULL #pblheight
@@ -75,55 +82,35 @@ npart = 100
 keep.hysplit.files = FALSE ## FALSE BY DEFAULT
 mc.cores = parallel::detectCores()
 
-flags = c()
 for(c in 1:(length(chunk_seq) - 1)) {
-  range = seq(chunk_seq[c], chunk_seq[c+1])
+  range = seq(chunk_seq[c]  + 1, chunk_seq[c+1])
   input.refs = data.table(input[range,], stringsAsFactors = FALSE)
   
-  iter = 0
-  while(iter < 3) {
-    hysp_raw <- run_disperser_parallel(input.refs = input.refs,
-                                       pbl.height = pbl.height,
-                                       species = species,
-                                       proc_dir = proc_dir,
-                                       overwrite = overwrite,
-                                       npart = npart,
-                                       keep.hysplit.files = keep.hysplit.files,
-                                       mc.cores = mc.cores)
+  hysp_raw <- tryCatch({
+      run_disperser_parallel(input.refs = input.refs,
+                                         pbl.height = pbl.height,
+                                         species = species,
+                                         proc_dir = proc_dir,
+                                         overwrite = overwrite,
+                                         npart = npart,
+                                         keep.hysplit.files = keep.hysplit.files,
+                                         mc.cores = mc.cores)
+    }, error = function(e) {system(paste0("echo Error_", 
+                                          args$year, "_", 
+                                          args$start, "_", 
+                                          args$end, "_", 
+                                          args$n, "_", 
+                                          c, "\n >> error.txt"))}
+    )
     
-    run_log <- unlist(hysp_raw)
-    flag <- length(grep("Error", run_log))
-    
-    if(flag == 0){
-      iter = 3
-    } else {
-      iter = iter + 1
-    }
-  }
-  
-  if(flag > 0) {
-    system(paste0("echo ", 
-                  paste(run_log[grep("Error", run_log)], collapse = ''), 
-                  " > ", 
-                  args$year, 
-                  "_", 
-                  c, 
-                  "_", 
-                  flag, 
-                  ".txt")
-           )
-  }
-  
-  flags = c(flags, flag)
+  run_log <- unlist(hysp_raw)
+  flag <- length(grep("Error", run_log))
+  system(paste0("echo Flag_", 
+                args$year, "_", 
+                args$start, "_", 
+                args$end, "_", 
+                args$n, "_",
+                c, "_",
+                flag, "\n >> flag.txt")
+  )
 }
-
-system(paste0("echo ", 
-              paste(flags, collapse = ''), 
-              " > flags_",
-              args$year, 
-              "_", 
-              args$n, 
-              "_",
-              args$c, 
-              ".txt")
-)
