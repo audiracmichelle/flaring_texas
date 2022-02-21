6d2b142b52a850e69e681a815315ec28e8b940aa
Michelle Audirac <m1ch3ll3@login3.stampede2.tacc.utexas.edu>
2022-02-21 12:57:19 -0600

    prepared subchunk launcher

---
+++ jobs/run_disperser/launcher_run_disperser.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#SBATCH -J run_disperser            # job name
+#SBATCH -N 24                        # number of nodes requested
+#SBATCH -n 24                       # total number of tasks to run in parallel
+#SBATCH -p skx-normal                  # queue (partition) 
+#SBATCH -t 01:30:00                 # run time (hh:mm:ss) 
+
+module load launcher
+export LAUNCHER_WORKDIR=/work/08317/m1ch3ll3/stampede2/flaring_texas
+export LAUNCHER_JOB_FILE=jobs/run_disperser/jobs_run_disperser
+export LD_PRELOAD=""
+
+${LAUNCHER_DIR}/paramrun
+
