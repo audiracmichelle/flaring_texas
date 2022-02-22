67167563926494dbf35d355479cc37013ee43bba
Michelle Audirac <audiracmichelle@michelles-air.lan>
2022-02-22 16:37:44 -0600

    made minimal changes to run_polygon_parcels function

--- lib/polygon_parcels_parallel.R
+++ lib/polygon_parcels_parallel.R
@@ -12,20 +12,14 @@ polygon_parcels_parallel <- function(
                      sf = sf, 
                      hysp_dir = hysp_dir, 
                      mc.cores = mc.cores)
-  
   rbindlist(polygon_parcels)
 }
-
-##### -------- flaring_texas adaptation >>>
-#d_trim <- na.omit(d_trim)
-##### -------- flaring_texas adaptation <<<
 
 # run_polygon_parcels
 run_polygon_parcels <- function(
   X, 
   sf, 
-  hysp_dir = hysp_dir, 
-  proc_dir = proc_dir
+  hysp_dir = hysp_dir
 ){
   X_tag <- paste("ID", X$ID, 
                  '| start_day', format(X$start_day, format = "%Y-%m-%d"), 
@@ -36,20 +30,22 @@ run_polygon_parcels <- function(
   hysp_dir_yr <- file.path(hysp_dir, X$year)
   hysp_dir_mo <- file.path(hysp_dir_yr,
                            formatC(month(X$start_day), width = 2, flag = '0'))
-  output_file <- path.expand(file.path(hysp_dir_mo,
+  hysp_file <- path.expand(file.path(hysp_dir_mo,
                                        paste0("hyspdisp_", 
                                               X$ID, "_", 
                                               X$start_day, "_", 
-                                              formatC(X$start_hour, width = 2, format = "d", flag = "0"),
+                                              formatC(X$start_hour, width = 2, 
+                                                      format = "d", flag = "0"),
                                               ".fst")))
   #### linking output
-  if(!file.exists(output_file)) {
+  if(!file.exists(hysp_file)) {
     out <- stop("Error hysplit not run for X:", X_tag)
   }
   
-  print(paste("Linking output from file:", output_file))
+  print(paste("Linking output from file:", hysp_file))
   
-  disp_df <- read.fst(output_file, as.data.table = TRUE)
+  disp_df <- read.fst(hysp_file, as.data.table = TRUE)
+  disp_df <- na.omit(disp_df)
   proj <- "+proj=aea +lat_1=20 +lat_2=60 +lat_0=40 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m"
   
   #parcels
@@ -73,7 +69,7 @@ run_polygon_parcels <- function(
   polygon_parcels$start_hour <- X$start_hour
   polygon_parcels<- data.table(polygon_parcels, stringsAsFactors = FALSE)
 
-  print(paste("linked output from", output_file))
+  print(paste("linked output from", hysp_file))
   return(polygon_parcels)
 }
 
