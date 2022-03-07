5baf2e172109040cb7514ed41c518cdde75324fe
Michelle Audirac <audiracmichelle@michelles-air.lan>
2022-03-07 14:46:08 -0600

    modified job params

--- jobs/run_disperser_w/jobs_parser.R
+++ jobs/run_disperser_w/jobs_parser.R
@@ -1,11 +1,23 @@
 n <- 100
 c <- seq(n)
-for(y in seq(2015,2020)) {
+for(y in seq(2015,2017)) {
   cat(paste("singularity exec $SCRATCH/disperser_latest.sif Rscript --vanilla jobs/run_disperser_w/run_disperser.R -y", 
             y, 
             "-n", n, 
             "-c", c, 
             "-s", 20, 
+            "-w '/work/08317/m1ch3ll3/stampede2/flaring_texas'\n", collapse = ""), 
+      file = paste0("./jobs_run_disperser_", y))
+}
+
+n <- 200
+c <- seq(n)
+for(y in seq(2018,2020)) {
+  cat(paste("singularity exec $SCRATCH/disperser_latest.sif Rscript --vanilla jobs/run_disperser_w/run_disperser.R -y", 
+            y, 
+            "-n", n, 
+            "-c", c, 
+            "-s", 25, 
             "-w '/work/08317/m1ch3ll3/stampede2/flaring_texas'\n", collapse = ""), 
       file = paste0("./jobs_run_disperser_", y))
 }
