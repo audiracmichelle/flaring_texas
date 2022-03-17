90941fb9c964e2795ef3e5137223d763566a594c
Michelle Audirac <audiracmichelle@michelles-air.lan>
2022-03-16 23:47:39 -0500

    modified link units jobs

---
+++ jobs/link_units/launcher_link_units_2016.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -J 2016_link_units               # job name
+#SBATCH -N 24                            # number of nodes requested
+#SBATCH -n 24                            # total number of tasks to run in parallel
+#SBATCH -p normal                        # queue (partition) 
+#SBATCH -t 01:30:00                      # run time (hh:mm:ss) 
+
+module load launcher
+export LAUNCHER_WORKDIR=/work/08317/m1ch3ll3/stampede2/flaring_texas
+export LAUNCHER_JOB_FILE=jobs/link_units/jobs_link_units_2016
+export LD_PRELOAD=""
+
+${LAUNCHER_DIR}/paramrun
